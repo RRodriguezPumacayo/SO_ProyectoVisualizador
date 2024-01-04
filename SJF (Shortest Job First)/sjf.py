7de4914ee1b63e4daee0d0f00474597f366becfb
EduardoRojasM <83146468+EduardoRojasM@users.noreply.github.com>
2024-01-03 23:12:41 -0500

    Procesos del SO - SJF

--- SJF (Shortest Job First)/sjf.py
+++ SJF (Shortest Job First)/sjf.py
@@ -1,23 +1,27 @@
 import os
 import psutil
+import time
 from tabulate import tabulate
 
 def get_running_processes():
     processes = []
 
-    for proc in psutil.process_iter(['pid', 'name', 'create_time']):
+    for proc in psutil.process_iter(['pid', 'name', 'create_time', 'cpu_percent', 'status']):
         try:
             pinfo = proc.info
             processes.append({
                 'pid': pinfo['pid'],
                 'name': pinfo['name'],
-                'create_time': pinfo['create_time']
+                'create_time': pinfo['create_time'],
+                'cpu_percent': pinfo['cpu_percent'],
+                'status': pinfo['status']
             })
         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
             pass
 
     return sorted(processes, key=lambda x: x['create_time'])
 
-def sjf_planificador():
+def sjf_scheduler():
     #Logica SJF
     return
+
